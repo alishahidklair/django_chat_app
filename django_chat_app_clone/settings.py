d37e4bc2534e0e90ad41eb857796e20dacaaa29a
Ali Shahid <alishahidjat@gmai.com>
2022-11-17 20:38:37 +0000

    commented out secret key from settings.py

--- django_chat_app_clone/settings.py
+++ django_chat_app_clone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o6#5gsv$9glocm%501_#(i(ejw9k7y=)47f$+pnry6(e99ur+k"
+# SECRET_KEY = "django-insecure-o6#5gsv$9glocm%501_#(i(ejw9k7y=)47f$+pnry6(e99ur+k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
